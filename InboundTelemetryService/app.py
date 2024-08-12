e54e363d475b210f3a8ba3ce39c1475c3d7a1ead
Paz Barda <paz.barda@intel.com>
2024-08-12 14:16:21 +0300

    basic relay REST API app

--- InboundTelemetryService/app.py
+++ InboundTelemetryService/app.py
@@ -1,1 +1,23 @@
-print('Hello World! InboundTelemetryService here')
+from flask import Flask, request, jsonify
+from kafka import KafkaProducer
+import json
+
+app = Flask(__name__)
+
+# producer = KafkaProducer(bootstrap_servers='kafka:9092',
+#                          value_serializer=lambda v: json.dumps(v).encode('utf-8'))
+
+@app.route('/process', methods=['POST'])
+def process():
+    data = request.get_json()
+    print('inbound telemetry message: ' + str(data))
+    # result = process_data(data)
+    # Send result to Kafka topic
+    # producer.send('processed_data', result)
+    return jsonify(data)
+
+def process_data(data):
+    return {"processed_data": data}
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80)
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # GreenOpsStem
 GreenOps service
+
+# Building and Running services in docker containers individually
+## InboundTelemetryService
+### Docker build
+`docker build -t gos-inbound-telemetry-service .`
+### Docker run
+`docker run -p 80:80 -e PYTHONUNBUFFERED=1 <image id>`
