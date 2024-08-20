fa837024418ce0362331cec9db27674351a7af02
Paz Barda <paz.barda@intel.com>
2024-08-20 17:46:00 +0300

    added kafka producer

--- InboundTelemetryService/app.py
+++ InboundTelemetryService/app.py
@@ -4,20 +4,21 @@ import json
 
 app = Flask(__name__)
 
-# producer = KafkaProducer(bootstrap_servers='kafka:9092',
-#                          value_serializer=lambda v: json.dumps(v).encode('utf-8'))
+producer = KafkaProducer(bootstrap_servers='kafka:9092',
+                         value_serializer=lambda v: json.dumps(v).encode('utf-8'))
+
+TARGET_KAFKA_TOPIC = 'inbound-telemetry'
 
 @app.route('/process', methods=['POST'])
 def process():
     data = request.get_json()
     print('inbound telemetry message: ' + str(data))
-    # result = process_data(data)
-    # Send result to Kafka topic
-    # producer.send('processed_data', result)
+    result = process_data(data)
+    producer.send(TARGET_KAFKA_TOPIC, result)
     return jsonify(data)
 
 def process_data(data):
-    return {"processed_data": data}
+    return {"payload": data}
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
--- README.md
+++ README.md
@@ -1,9 +1,19 @@
 # GreenOpsStem
 GreenOps service
 
+# Docker Compose
+`docker-compose up --build`
+
 # Building and Running services in docker containers individually
+
 ## InboundTelemetryService
+
 ### Docker build
-`docker build -t gos-inbound-telemetry-service .`
+```
+cd InboundTelemetryService/
+docker build -t gos-inbound-telemetry-service .
+```
+
 ### Docker run
+
 `docker run -p 80:80 -e PYTHONUNBUFFERED=1 <image id>`
